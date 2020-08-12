c51755ad506dd4efaa2a75b426dd87e7a2def893
Nerly Ton <nerlyton@gmail.com>
2020-08-12 19:46:54 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -18,13 +18,13 @@ class Patient
   end
   
   def appointments
-    Appointments.all.select do |appointment|
+    Appointment.all.select do |appointment|
       appointment.patient == self
     end
   end
   
   def doctors
-    Appointments.all.map do |appointment|
+    Appointment.all.map do |appointment|
       appointment.doctor
     end
   end
